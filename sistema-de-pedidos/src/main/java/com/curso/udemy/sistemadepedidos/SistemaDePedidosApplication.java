3114209048619289c42bebcb4b7d4be98e7f06e0
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-02-10 07:45:59 -0300

    Criando projeto

---
+++ sistema-de-pedidos/src/main/java/com/curso/udemy/sistemadepedidos/SistemaDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.udemy.sistemadepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+
+}
