ffd56f5cde3b868de288068362ca41de2595d2d6
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-02-25 09:36:21 -0300

    Adicionando instaciaçao de objetos

--- sistema-de-pedidos/src/main/java/com/curso/udemy/sistemadepedidos/SistemaDePedidosApplication.java
+++ sistema-de-pedidos/src/main/java/com/curso/udemy/sistemadepedidos/SistemaDePedidosApplication.java
@@ -1,13 +1,34 @@
 package com.curso.udemy.sistemadepedidos;
 
+import java.util.Arrays;
+
+import com.curso.udemy.sistemadepedidos.domain.Categoria;
+import com.curso.udemy.sistemadepedidos.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaDePedidosApplication {
+public class SistemaDePedidosApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDePedidosApplication.class, args);
 	}
-	
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+
+		repository.saveAll(Arrays.asList(cat1,cat2));
+
+		
+	}
+
 }
